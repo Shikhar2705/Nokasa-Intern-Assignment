f61764a0cd15463f50a5d006f12a207eb8d20a97
Shikhar2705 <shikhar.saxena2705@gmail.com>
2025-09-15 00:05:21 +0530

    API Created

--- server.js
+++ server.js
@@ -7,7 +7,16 @@ app.use(express.json());
 const userVersion1 = new Map(); // In-memory storage for email (Version 1)
 const userVersion2 = new Map(); // In-memory storage for phone number (Version 2)
 
+app.use('/api/v1', require('./src/routes/version1')(userVersion1));
+app.use('/api/v2', require('./src/routes/version2')(userVersion2));
 
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send({
+        status: 'error',
+        message: 'Something went wrong!'
+    });
+});
 
 // Starting the server
 app.listen(port, () => {
