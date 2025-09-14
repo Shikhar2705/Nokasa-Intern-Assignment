42e850ec6209ae04e6104fe1d5f8f74ddf6f2932
Shikhar2705 <shikhar.saxena2705@gmail.com>
2025-09-14 23:19:38 +0530

    Initialize Server

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+
+app.use(express.json());
+
+const userVersion1 = new Map(); // In-memory storage for email (Version 1)
+const userVersion2 = new Map(); // In-memory storage for phone number (Version 2)
+
+// Starting the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
+module.exports = app; // Exporting app for testing purposes
